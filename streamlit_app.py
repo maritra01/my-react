dd5683d9a90166087512c736a4c0b6c6d3dcb39a
maritra01 <99782097+maritra01@users.noreply.github.com>
2024-07-26 15:12:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,60 +1,34 @@
-# import the streamlit library
-import streamlit as st
+import streamlit as st 
+import pandas as pd 
+import time 
+from streamlit_extras.let_it_rain import rain 
 
-# give a title to our app
-st.title('Welcome to BMI Calculator')
 
-# TAKE WEIGHT INPUT in kgs
-weight = st.number_input("Enter your weight (in kgs)")
 
-# TAKE HEIGHT INPUT
-# radio button to choose height format
-status = st.radio('Select your height format: ',
-				('cms', 'meters', 'feet'))
+df = pd.read_csv("iris.csv") 
 
-# compare status value
-if(status == 'cms'):
-	# take height input in centimeters
-	height = st.number_input('Centimeters')
+col = st.sidebar.selectbox("Select any column", df.columns) 
 
-	try:
-		bmi = weight / ((height/100)**2)
-	except:
-		st.text("Enter some value of height")
 
-elif(status == 'meters'):
-	# take height input in meters
-	height = st.number_input('Meters')
+col = st.sidebar.multiselect("Select any column", df.columns) 
 
-	try:
-		bmi = weight / (height ** 2)
-	except:
-		st.text("Enter some value of height")
+prg = st.progress(0) 
 
-else:
-	# take height input in feet
-	height = st.number_input('Feet')
+for i in range(100): 
+	time.sleep(0.001) 
+	prg.progress(i+1) 
 
-	# 1 meter = 3.28
-	try:
-		bmi = weight / (((height/3.28))**2)
-	except:
-		st.text("Enter some value of height")
-
-# check if the button is pressed or not
-if(st.button('Calculate BMI')):
-
-	# print the BMI INDEX
-	st.text("Your BMI Index is {}.".format(bmi))
-
-	# give the interpretation of BMI index
-	if(bmi < 16):
-		st.error("You are Extremely Underweight")
-	elif(bmi >= 16 and bmi < 18.5):
-		st.warning("You are Underweight")
-	elif(bmi >= 18.5 and bmi < 25):
-		st.success("Healthy")
-	elif(bmi >= 25 and bmi < 30):
-		st.warning("Overweight")
-	elif(bmi >= 30):
-		st.error("Extremely Overweight")
+# Raining Snowflake. 
+	
+rain( 
+	emoji="", 
+	
+	# the size of each snowflake 
+	font_size=20, 
+	# speed of raining 
+	falling_speed=3, 
+	
+	
+	# for how much time it will fall 
+	animation_length="infinite", 
+) 
